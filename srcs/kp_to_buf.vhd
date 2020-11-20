5ae44d3a6464059e7cc7144d139b888420984209
John Greathouse <47994557+jgreatho@users.noreply.github.com>
2020-11-20 09:43:48 -0500

    Code retired, some comments added

--- srcs/kp_to_buf.vhd
+++ srcs/kp_to_buf.vhd
@@ -1,16 +1,18 @@
+-- This code was used with the Keyboard Testbench to ensure that keypresses are read correctly\
+-- Then entites are now used in the top 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity kp_to_buf is
-    Port ( clk : in STD_LOGIC;
+    Port ( clk : in STD_LOGIC; -- BASYS3 Clock (100Mhz expected)
            reset : in STD_LOGIC;
            ps2_clk : in STD_LOGIC;
            ps2_data : in STD_LOGIC;
            
            ASCII : out STD_LOGIC_VECTOR(7 downto 0);
-           newpress : out STD_LOGIC;
-           clk_out : out STD_LOGIC);
+           newpress : out STD_LOGIC; -- Held for 50 BASYS3 clock cycles
+           clk_out : out STD_LOGIC); -- A copy of the clock signal
 end kp_to_buf;
 
 architecture bhv of kp_to_buf is
